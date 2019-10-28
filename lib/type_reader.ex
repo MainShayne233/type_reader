2442813db914e19e911dc547324aaeeced651b86
MainShayne233 <shaynetremblay@gmail.com>
2019-10-28 15:40:58 -0400

    Init

---
+++ lib/type_reader.ex
@@ -0,0 +1,18 @@
+defmodule TypeReader do
+  @moduledoc """
+  Documentation for TypeReader.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TypeReader.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/type_reader_test.exs
@@ -0,0 +1,8 @@
+defmodule TypeReaderTest do
+  use ExUnit.Case
+  doctest TypeReader
+
+  test "greets the world" do
+    assert TypeReader.hello() == :world
+  end
+end
