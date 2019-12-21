d844fab25c376db74a07d8011072b6f78d741a0e
MainShayne233 <shaynetremblay@gmail.com>
2019-12-21 15:41:37 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TypeReader.MixProject do
   def project do
     [
       app: :type_reader,
-      version: "0.0.3",
+      version: "0.0.4",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
